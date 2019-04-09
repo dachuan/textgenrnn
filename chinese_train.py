05017c9e14b06861b62927315cbc8bee7767ae42
dcthe <tianiao@126.com>
2019-04-09 10:01:23 +0800

    good

---
+++ chinese_train.py
@@ -0,0 +1,14 @@
+from textgenrnn import textgenrnn
+textgen = textgenrnn(name="c_p")
+
+textgen.reset()
+textgen.train_from_file(
+	file_path = './datasets/c_poem.txt',
+	new_model = True,
+	num_epochs = 30,
+	word_level = False,
+	rnn_bidirectional = True,
+	max_length = 10,
+	train_size = 0.8,
+	dropout = 0.2
+)
